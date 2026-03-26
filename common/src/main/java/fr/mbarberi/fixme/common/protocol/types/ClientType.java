7c477577af6f6da9dea92b40fbb8b8d0fe442b33
Matthieu Barberis <foobarberis@gmail.com>
2026-03-26 16:53:51 +0100

    Fix swapped enums

--- common/src/main/java/fr/mbarberi/fixme/common/protocol/types/ClientType.java
+++ common/src/main/java/fr/mbarberi/fixme/common/protocol/types/ClientType.java
@@ -1,10 +1,7 @@
 package fr.mbarberi.fixme.common.protocol.types;
 
 public enum ClientType {
-  /* router */
-  BAD_CHECKSUM,
-  UNKNOWN_DESTINATION,
-  MALFORMED_MESSAGE,
-  /* market */
-  UNKNOWN_SYMBOL, INSUFFICIENT_QUANTITY
+  BROKER,
+  MARKET,
+  ROUTER
 }
--- common/src/main/java/fr/mbarberi/fixme/common/protocol/types/RejectReason.java
+++ common/src/main/java/fr/mbarberi/fixme/common/protocol/types/RejectReason.java
@@ -1,7 +1,10 @@
 package fr.mbarberi.fixme.common.protocol.types;
 
 public enum RejectReason {
-  BROKER,
-  MARKET,
-  ROUTER
+  /* router */
+  BAD_CHECKSUM,
+  UNKNOWN_DESTINATION,
+  MALFORMED_MESSAGE,
+  /* market */
+  UNKNOWN_SYMBOL, INSUFFICIENT_QUANTITY
 }
